fa554499a3961c1732fd7339821d3fc61a8b2e21
Dávid Vörös <itkutya@student.elte.hu>
2022-05-20 21:21:17 +0200

    try-catch settup...

--- TheFanGame/main.cpp
+++ TheFanGame/main.cpp
@@ -1,4 +1,19 @@
+#include <exception>
+#include <iostream>
+
 int main()
 {
+	try
+	{
+
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what() << '\n';
+	}
+	catch(...)
+	{
+		std::cout << "Somehing went wrong...\n";
+	}
 	return 0;
 }
