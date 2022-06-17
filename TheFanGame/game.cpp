62ee22c6b045685649f2471fb7412d48052cf4e6
Dávid Vörös <itkutya@student.elte.hu>
2022-06-17 22:19:24 +0200

    idk, still lags like crazy...

--- TheFanGame/game.cpp
+++ TheFanGame/game.cpp
@@ -16,9 +16,12 @@ const void game::init(sf::RenderWindow& window)
 
     this->m_texture = &this->m_context->g_resources.getTexture(0);
 
-    this->m_entities.push_back(std::make_unique<quad>(sf::Vector2f((float)this->m_map.mapSize.x, (float)this->m_map.mapSize.y), 
-                                                      sf::Vector2f((float)(std::rand() % 100), (float)(std::rand() % 100)), 
-                                                      sf::Color::Green));
+    for (std::size_t i = 0; i < 10; ++i)
+    {
+        this->m_entities.push_back(std::make_unique<quad>(sf::Vector2f((float)this->m_map.mapSize.x, (float)this->m_map.mapSize.y),
+            sf::Vector2f((float)(std::rand() % 50), (float)(std::rand() % 50)),
+            sf::Color::Green));
+    }
 
     this->zBuffer.resize(window.getSize().x);
 }
@@ -41,10 +44,12 @@ const void game::processEvent(const sf::Event& event) noexcept
 
 const void game::update(sf::RenderWindow& window, const sf::Time& dt) noexcept
 {
+    /*
     if (1.f / dt.asSeconds() > 70.f)
         std::cout << "high\n";
     else if(1.f / dt.asSeconds() < 50.f)
         std::cout << "low\n";
+    */
 
     ImGui::SFML::Update(window, dt);
     ImGui::Begin("FPS Counter", 0, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize);
@@ -66,22 +71,19 @@ const void game::update(sf::RenderWindow& window, const sf::Time& dt) noexcept
         this->m_ray.castRay(this->m_player, this->m_map, window.getSize().x, window.getSize().y, i);
         this->zBuffer[i] = this->m_ray.perpWallDist * ((this->m_map.mapSize.x + this->m_map.mapSize.y) / 2.f);
     }
-    /*
+    
     std::vector<int> spriteOrder(this->m_entities.size());
     std::vector<float> spriteDistance(this->m_entities.size());
 
     for (std::uint32_t i = 0; i < this->m_entities.size(); ++i)
     {
         spriteOrder[i] = i;
-        spriteDistance[i] = ((this->m_player.getPosition().x - this->m_entities[i]->getPosition().x) * (this->m_player.getPosition().x - this->m_entities[i]->getPosition().x)
-                          + (this->m_player.getPosition().y - this->m_entities[i]->getPosition().y) * (this->m_player.getPosition().y - this->m_entities[i]->getPosition().y));
+        spriteDistance[i] = std::hypotf(this->m_player.getPosition().x - this->m_entities[i]->getPosition().x, this->m_player.getPosition().y - this->m_entities[i]->getPosition().y);
     }
     this->sortSprites(spriteOrder, spriteDistance, this->m_entities.size());
-    */
+    
     for (std::uint32_t i = 0; i < this->m_entities.size(); ++i)
-    {
-        this->m_entities[i]->update(this->m_player, window.getSize(), this->zBuffer);
-    }
+        this->m_entities[spriteOrder[i]]->update(this->m_player, window.getSize(), this->zBuffer);
 }
 
 const void game::draw(sf::RenderWindow& window) noexcept
