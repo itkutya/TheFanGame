ef8ae7f023d65413baac83b02d4d514b0cbee435
itkutya <itkutya@gmail.com>
2022-11-21 20:16:25 +0100

    test...

--- WebsiteForTheFanGame/login.php
+++ WebsiteForTheFanGame/login.php
@@ -1,3 +1,18 @@
-<?php
-    echo "Hello";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Hello";
+            echo date('j-m-y, h:i:s');
+        ?>
+    </h1>
+</body>
+</html>
